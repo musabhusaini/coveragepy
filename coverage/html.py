ef9d67de56eec42beb51f410159dcb55cefdb9c0
Mus'ab Husaini <mhusaini@hbku.edu.qa>
2018-11-22 13:36:08 +0300

    Fixing bug 'str' object has no attribute 'decode'

--- coverage/html.py
+++ coverage/html.py
@@ -99,8 +99,8 @@ class HtmlReporter(Reporter):
             self.not_inline_styles = False
 
             # reading the css stylesheet
-            f = open(os.path.join(os.path.dirname(__file__), *["htmlfiles", "style.css"]))
-            self.css_styles = f.read().decode('utf-8').strip()
+            f = open(os.path.join(os.path.dirname(__file__), *["htmlfiles", "style.css"]), encoding='utf-8')
+            self.css_styles = f.read().strip()
             f.flush()
             f.close()
         except Exception as e:
